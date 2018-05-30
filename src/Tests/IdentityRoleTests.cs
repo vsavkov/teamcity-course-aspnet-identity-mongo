12d49556ce06779a5d1ccdeec71e296e403d7489
Victor Savkov <vs@server96.local>
2018-05-30 17:26:40 -0400

    Fixed test

--- src/Tests/IdentityRoleTests.cs
+++ src/Tests/IdentityRoleTests.cs
@@ -36,7 +36,7 @@
 			var role = new IdentityRole(name);
 
 			Expect(role.Name, Is.EqualTo(name));
-			Expect(role.Name, Is.EqualTo("notadmin"));
+			// Expect(role.Name, Is.EqualTo("notadmin"));
 		}
 
 		[Test]
